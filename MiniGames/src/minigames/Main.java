4959661c4e96990caa2dc0df3b1a1ae9e2297bd4
lin <100304367+longstorycourt@users.noreply.github.com>
2024-07-19 21:15:35 +0700

    delete new LogIn form

--- MiniGames/src/minigames/Main.java
+++ MiniGames/src/minigames/Main.java
@@ -1,7 +1,7 @@
 package minigames;
 public class Main {
     public static void main(String[] args){
-        //new Login();
+        new Login();
         //new Homepage();
         //new GuessNum();
     }
